8ef130ce8a9897ea6b3a0b41793eafbd7daa3ab4
Gabriel Debona <gabriel.debona@gmail.com>
2025-07-14 12:18:56 -0300

    fix example

--- pocs/webGL/tutorial/main.js
+++ pocs/webGL/tutorial/main.js
@@ -1,38 +1,35 @@
 const vertexShaderSource = `#version 300 es
 #pragma vscode_glsllint_stage: vert
 
-// variáveis enviadas do JavaScript para o shader
-uniform float uPointSize; // tamanho do ponto em px
-uniform vec2 uPosition; // vetor 2d com a posição do ponto
+uniform float uPointSize;
+uniform vec2 uPosition;
 
-void main() // funcao principal
+void main()
 {
-    gl_PointSize = uPointSize; // define o tamanho do ponto
-    gl_Position = vec4(uPosition, 0.0, 1.0); // (x, y, z, w)
-    // define a posição final como vetor 4D (x, y, z, w)
-    // Converte a posição 2D para 4D adicionando z=0.0 e w=1.0
+    gl_PointSize = uPointSize;
+    gl_Position = vec4(uPosition, 0.0, 1.0);
 }`;
 
 const fragmentShaderSource = `#version 300 es
 #pragma vscode_glsllint_stage: frag
 
-precision mediump float; // Define precisão média para números float
+precision mediump float;
 
-out vec4 fragColor; // Variável de saída que define a cor final do pixel
+out vec4 fragColor;
 
 void main()
 {
-    fragColor = vec4(1.0, 0.0, 0.0, .5); // Define a cor como vermelho sólido (R=1.0, G=0.0, B=0.0, A=1.0)
+    fragColor = vec4(1.0, 0.0, 0.0, .5);
 }`;
 
-const canvas = document.querySelector('canvas'); // pega o evemento do dom
-const gl = canvas.getContext('webgl2'); // cria um context webgl2
-const program = gl.createProgram(); // cria um programa
+const canvas = document.querySelector('canvas');
+const gl = canvas.getContext('webgl2');
+const program = gl.createProgram();
 
-const vertexShader = gl.createShader(gl.VERTEX_SHADER); // cria um objeto vertex shader
-gl.shaderSource(vertexShader, vertexShaderSource); // atribui o codigo fonte do shader ao objeto
-gl.compileShader(vertexShader); // combila o shader using the CPU
-gl.attachShader(program, vertexShader); // Anexa o shader compilado ao programa
+const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+gl.shaderSource(vertexShader, vertexShaderSource);
+gl.compileShader(vertexShader);
+gl.attachShader(program, vertexShader);
 
 const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER); 
 gl.shaderSource(fragmentShader, fragmentShaderSource);
@@ -46,7 +43,7 @@ gl.useProgram(program);
 const uPositionLoc = gl.getUniformLocation(program, 'uPosition');
 const uPointSizeLoc = gl.getUniformLocation(program, 'uPointSize');
 
-gl.uniform1f(uPointSizeLoc, 100); // Define tamanho do ponto: 100 pixels
-gl.uniform2f(uPositionLoc, 0, 0); // centro horizontal, centro vertical
+gl.uniform1f(uPointSizeLoc, 100);
+gl.uniform2f(uPositionLoc, 0, 0);
 
-gl.drawArrays(gl.POINTS, 0, 1); // Here use the GPU
+gl.drawArrays(gl.POINTS, 0, 1);
