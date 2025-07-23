62d58e1ae2824885bf032854bd15ce535aa2dceb
Gabriel Debona <gabriel.debona@gmail.com>
2025-07-23 16:18:13 -0300

    added new render and workloop

--- pocs/react/own-react/src/render.ts
+++ pocs/react/own-react/src/render.ts
@@ -1,23 +1,54 @@
-export function render(element, container) {
-  if (typeof element === "string" || typeof element === "number") {
-    const textNode = document.createTextNode(element);
-    container.appendChild(textNode);
-    return;
+// export function render(element, container) {
+//   if (typeof element === "string" || typeof element === "number") {
+//     const textNode = document.createTextNode(element);
+//     container.appendChild(textNode);
+//     return;
+//   }
+//
+//   const dom = document.createElement(element.type);
+//
+//   if (element.props) {
+//     for (const [key, value] of Object.entries(element.props)) {
+//       if (key === "children") continue;
+//       dom.setAttribute(key, value);
+//     }
+//   }
+//
+//   const children = element.props?.children || [];
+//   (Array.isArray(children) ? children : [children]).forEach(child =>
+//     render(child, dom)
+//   );
+//
+//   container.appendChild(dom);
+// }
+
+let nextUnitOfWork = null;
+let wipRoot = null;
+
+function render(element, container) {
+  wipRoot = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+    parent: null,
+  };
+
+  nextUnitOfWork = wipRoot;
+  requestIdleCallback(workLoop);
+}
+
+function workLoop(deadline) {
+  let shouldYield = false;
+
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    shouldYield = deadline.timeRemaining() < 1;
   }
 
-  const dom = document.createElement(element.type);
-
-  if (element.props) {
-    for (const [key, value] of Object.entries(element.props)) {
-      if (key === "children") continue;
-      dom.setAttribute(key, value);
-    }
+  if (!nextUnitOfWork && wipRoot) {
+    commitRoot();
   }
 
-  const children = element.props?.children || [];
-  (Array.isArray(children) ? children : [children]).forEach(child =>
-    render(child, dom)
-  );
-
-  container.appendChild(dom);
+  requestIdleCallback(workLoop);
 }
