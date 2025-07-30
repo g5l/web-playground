15923478bd335a9f450850cf779f50d5993eca3a
Gabriel Debona <gabriel.debona@gmail.com>
2025-07-29 22:25:40 -0300

    update render and create element

--- pocs/react/own-react/src/createElement.ts
+++ pocs/react/own-react/src/createElement.ts
@@ -17,3 +17,13 @@ export function createElement(
     }
   };
 }
+
+export function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  }
+}
